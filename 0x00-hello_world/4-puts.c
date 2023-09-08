b69a019c28b55c0c729d14da4655518d6af47f4a
Henawy <Henawy@email>
2023-09-08 05:43:28 +0200

    Add File

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+* main - Entry point function
+* Description: 'First task that prints string'
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
