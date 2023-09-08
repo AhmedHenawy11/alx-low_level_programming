7bc92c2fd8d5eebcbd02492f0834804cf6ce0891
Henawy <Henawy@email>
2023-09-08 05:49:04 +0200

    Add File

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point function
+* Description: 'Seconed task that prints string'
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
