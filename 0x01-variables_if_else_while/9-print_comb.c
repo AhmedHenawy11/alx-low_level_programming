18af058e21e4fa9a970b14e53b49882c145b321e
Henawy <Henawy@email>
2023-09-09 01:46:21 +0200

    diagonally

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n < 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+		putchar('\n');
+		return (0);
+}
+
