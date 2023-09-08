1b5e409f6ce7cbe410116c40b7efe2aa51ab7f15
Henawy <Henawy@email>
2023-09-09 01:11:12 +0200

    Rutgers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789abcdef\n");
+	return (0);
+}
+
