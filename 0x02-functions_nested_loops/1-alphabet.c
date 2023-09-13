173a5b6c2500e260acecfb97a8a7d28b82b180dc
Henawy <Henawy@email>
2023-09-13 04:32:50 +0200

    affectation

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the alphabet in lowercase,
  * followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void);
 {
 	char ch;
 
