72a7e0e0edf23b5530a08e212c0ee2f550719d92
Henawy <Henawy@email>
2023-09-15 05:18:21 +0200

    bacteriological

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest = a;
+
+	if (b > a && c > a)
+	{
+		largest = b;
+		if (c > b)
+		{
+			largest = c;
+		}
+	}
+	return (largest);
+}
