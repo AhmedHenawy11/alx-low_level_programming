a756c668481cff62b6944936e809cc8a2c71a43f
Henawy <Henawy@email>
2023-09-19 00:07:57 +0200

    perfected

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,19 +5,17 @@
  * @n: the number to be tested
  * Return: 1 if c is a letter, 0 otherwise
  */
-	int print_sign(int n)
+int print_sign(int n)
 	{
 		if (n > 0)
 		{
 			_putchar('+');
 			return (1);
-		}
-		else if (n == 0)
+		} else if (n == 0)
 		{
-			_putchar('48');
+			_putchar(48);
 			return (0);
-		}
-		else if (n < 0)
+		} else if (n < 0)
 		{
 			_putchar('-');
 		}
