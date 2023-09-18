c0f00922388c409b2534e56898782b9acdeedec1
Henawy <Henawy@email>
2023-09-18 23:51:47 +0200

    buffs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - test the sign of the number
+ * @n: the number to be tested
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar("-");
+		return (-1);
+	}
+	if (n == 0)
+	{
+		_putchar("0");
+		return (0);
+	}
+}
