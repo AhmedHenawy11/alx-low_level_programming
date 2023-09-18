e62c18ef6f58b94ad8110c29ad49473d0113185e
Henawy <Henawy@email>
2023-09-19 00:44:47 +0200

    detracted

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,33 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int r;
+    int r;
 
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+    r = _abs(-1);
+    printf("%d\n", r);
+    r = _abs(0);
+    printf("%d\n", r);
+    r = _abs(1);
+    printf("%d\n", r);
+    r = _abs(-98);
+    printf("%d\n", r);
+    return (0);
 }
