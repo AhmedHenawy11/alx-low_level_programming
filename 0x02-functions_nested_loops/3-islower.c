b18feb0c05d773c2aa9d171a3cccda4c0497dda5
Henawy <Henawy@email>
2023-09-18 20:41:53 +0200

    promote

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,16 +7,8 @@
  */
 int _islower(int c)
 {
-	char ch;
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		for (ch = 'z'; ch <= 'a'; ch--)
-		{
-			putchar(ch);
-		}
-	}
-	putchar('\n');
-	return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
