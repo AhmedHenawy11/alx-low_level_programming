060c4d272ec563dbe39302fbf5d9ee5e49655b1c
Henawy <Henawy@email>
2023-09-18 23:56:42 +0200

    Lafayette's

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar("-");
+		_putchar('-');
 		return (-1);
 	}
 	if (n == 0)
 	{
-		_putchar("0");
+		_putchar('48');
 		return (0);
 	}
 }
