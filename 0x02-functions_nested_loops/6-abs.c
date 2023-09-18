db852a35af38d9adbd45701320391065d176f9f1
Henawy <Henawy@email>
2023-09-19 00:36:19 +0200

    asylums

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,17 +2,15 @@
 
 /**
  * _abs - print_abslute value of number - test the sign of the number
- * @int: the number to be tested
+ * @r: the number to be tested
  * Return: aboluse vlaue of number
  */
-int _abs(int)
+int _abs(int r)
 {
-	int number;
-
-	if (number < 0)
-	return (-number);
-	else if (number == 0)
+	if (r < 0)
+	return (-r);
+	else if (r == 0)
 	return (0);
 	else
-	return (number);
+	return (r);
 }
