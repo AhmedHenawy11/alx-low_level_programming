c915a1d3820f96dc3f1cc463ac24505948928355
Henawy <Henawy@email>
2023-09-19 02:25:37 +0200

    cannabis's

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - sum function
+ * @a: first int
+ * @b: seconed int
+ * Return: sum of two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,4 +11,5 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+int add(int, int);
 #endif /* MAIN_H */
