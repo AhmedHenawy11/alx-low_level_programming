ddc90896679355dbbab806752e1dff009bdee665
Henawy <Henawy@email>
2023-09-22 11:07:03 +0200

    annexing

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
-* 
-* 
+* rev_string - printing in revrse
+* @s: the string
 */
+
+void print_rev(char *s)
+{
+	int l = 0;
+	int ll = 0;
+	char *r;
+
+	while (s[l])
+	{
+		l++;
+	}
+	while (l--)
+	{
+		r[ll] = s[l];
+		ll++;
+	}
+	*s = *r;
+}
