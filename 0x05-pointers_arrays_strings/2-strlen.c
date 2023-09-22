637857838f3382416897469c8a98380a5d2c4ead
Henawy <Henawy@email>
2023-09-22 10:19:26 +0200

    Cu

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
 
 /**
-* 
-* 
+* _strlen-return lenth of pointer of characters
+* description: return lenth of pointer of characters
+* @s: characters pointer
+* Return: int of lenth
 */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
