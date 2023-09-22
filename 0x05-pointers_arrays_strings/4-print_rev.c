451c55f9c00c79c485a1527f9e26b9b8068c436b
Henawy <Henawy@email>
2023-09-22 10:45:37 +0200

    yack's

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-
+#include <string.h>
 /**
-* 
-* 
+* print_rev - printing in revrse
+* @s: the string
 */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	for (int i = len - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+}
