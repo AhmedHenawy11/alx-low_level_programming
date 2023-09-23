69a7fe8fca4a23a0690e1920957c9975767107a3
Henawy <Henawy@email>
2023-09-23 05:12:58 +0200

    Sabrina

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,6 +2,25 @@
 #include "main.h"
 
 /**
-* 
-* 
+* print_array - pint numbers of n
+* @n: lenth to print
+*@a: Array
 */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i == n - 1)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+		i++;
+	}
+}
