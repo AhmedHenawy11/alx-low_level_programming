079286fc1974282f3ba458d226d5cf77f68418df
Henawy <Henawy@email>
2023-09-23 05:31:15 +0200

    physic's

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,6 +2,21 @@
 #include "main.h"
 
 /**
-* 
-* 
+* _strcpy - copying a string
+* @dest: to be returned
+* @src: the source of copy
+* description: copying a string to another
+* Return: a string
 */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
