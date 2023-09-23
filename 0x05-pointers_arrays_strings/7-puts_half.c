6026dd3d1e08188df5792fed9d6042b5f3cbe76c
Henawy <Henawy@email>
2023-09-23 04:35:33 +0200

    shallow

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,6 +2,23 @@
 #include "main.h"
 
 /**
-* 
-* 
+* puts_half - print half of string.
+* @str:the string
 */
+
+void puts_half(char *str)
+{
+	int j = 0;
+	int i = 0;
+
+	while (str[j])
+	{
+		j++;
+	}
+	i = j / 2;
+	while (i < j)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
