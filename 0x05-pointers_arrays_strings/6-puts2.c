7a78f021db24421aa9b7cd26503a1a9aa60d60f4
Henawy <Henawy@email>
2023-09-23 04:16:54 +0200

    investigators

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,6 +2,20 @@
 #include "main.h"
 
 /**
-* 
-* 
+* puts2 - prit every other character.
+* @str: the string.
 */
+
+void puts2(char *str)
+{
+	int j = 0;
+
+	while (str[j])
+	{
+		j++;
+	}
+	for (int i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
