a8dad5a1e0b2a203fba868798ae2650b0709b28f
Henawy <you@example.com>
2023-09-27 09:38:11 +0200

    intravenous's

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,20 +9,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
+	int i;
+	int j;
 
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	int j = 0;
-
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
