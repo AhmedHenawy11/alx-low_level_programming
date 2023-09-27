c6ada654e18bc7e6aff4b0a6e166c0e6ecfc7c01
Henawy <you@example.com>
2023-09-27 09:37:30 +0200

    calories

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i = 0;
 
-	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	j = 0;
+	int j = 0;
+
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-
 	dest[i] = '\0';
 	return (dest);
 }
