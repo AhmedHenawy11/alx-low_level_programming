3eb684e17bf0e0604b836e028c4a00e977167b10
Henawy <you@example.com>
2023-09-27 09:33:35 +0200

    acquired

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - conacatinate strings
+ * @dest: first string
+ * @src: Secone one
+ * Return: dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	int j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
