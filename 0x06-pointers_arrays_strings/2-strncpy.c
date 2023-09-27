504261bf3aa51f2c63040d7b8ab4877840cb2de8
Henawy <you@example.com>
2023-09-27 10:25:51 +0200

    interweaved

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - conacatinate strings
+ * @dest: first string
+ * @src: Secone one
+ * @n:input value
+ * Return: dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
