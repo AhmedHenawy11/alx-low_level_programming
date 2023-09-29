d5f81f1319b52d7f7cba625b7b5f0600ed49fd4a
Henawy <Henawy@email>
2023-09-29 04:26:30 +0200

    insubordination's

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _pow_recursion - return x raised to the power of y
+ * @x: nbr duplicated
+ * @y: power of a nbr
  *
- * Return: Always 0.
+ * Return: final result
  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
