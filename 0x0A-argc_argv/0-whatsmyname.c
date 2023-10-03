50dbc016178f241ecd27159b2a50857f7791586b
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-03 14:27:53 +0300

    talk's

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - argc, argv
+ * description: return the name of program
+ * @argc: argumnts count
+ * @argv: arguments passed
+ * Return: (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
