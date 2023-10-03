def693870ec139107a4d85320eaa10cab5e1c977
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-03 15:16:46 +0300

    philatelists

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,51 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-
-/**
- * atoi -  string to integer
- * @s: string to be converted
- *
- * Return: the int converted
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
 /**
  * main - argc, argv
  * description: return the command line arguments
@@ -53,15 +8,13 @@ int _atoi(char *s)
  * @argv: arguments passed
  * Return: (0)
 */
+
 int main(int argc, char *argv[])
 {
-	int a = _atoi(argv[1]);
-	int b = _atoi(argv[2]);
-
-	printf("%d\n", a * b);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	if (argc != 3)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		return (1);
 	}
 	return (0);
