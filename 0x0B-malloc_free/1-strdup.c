08a76774801d973037da7c11bed903ca45cf7c19
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-04 18:28:53 +0300

    Sudoku

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - creates an array of chars,
+ * and initializes it with a specific char.
+ * @str: string.
+ * Return: pointer or NULL.
+ */
+char *_strdup(char *str)
+{
+	char *new_s;
+	int i = 0;
+	int j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	new_s = malloc(sizeof(char) * (i + 1));
+
+	if (new_s == NULL)
+	return (NULL);
+	for (j = 0; str[j]; j++)
+	{
+		new_s[j] = str[j];
+	}
+	return (new_s);
+}
