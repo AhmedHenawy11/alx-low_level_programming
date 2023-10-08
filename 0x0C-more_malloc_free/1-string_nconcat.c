cf97260c3ddddf545a35882c5f546a399fc27c1b
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-08 10:58:19 +0300

    Ger

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,53 +3,43 @@
 #include <stdlib.h>
 #include <limits.h>
 /**
- * malloc_checked - check the code
- *@b: int
+ * string_nconcat - check the code
+ * @s1: first string
+ * @s2: seconed string passed
+ * @n: int
  * Return: ptr(pointer to heap).
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i , j , l, k = 0;
-    char *s;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-    while (s1 && s1[i])
-    {
-        i++;
-    }
-    while (s2 && s2[k])
-    {
-        k++;
-    }
-    if (n < k)
-    {
-        s = malloc(sizeof(char) * (i + n + 1));
-    }
-    else
-    {
-        s = malloc(sizeof(char) * (i + k + 1));
-    }
-    if (!s)
-        return (NULL);
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-    while (l < i)
-    {
-        s[l] = s1[l];
-        l++;
-    }
-    while (n < k && l < i + n)
-    {
-        s[l] = s2[j];
-        j++;
-        l++;
-    }
-    while (n >= k && l < i + k)
-    {
-        s[l] = s2[j];
-        j++;
-        l++;
-    }
-    s[l] = '\0';    
-    
-    return (s);
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
