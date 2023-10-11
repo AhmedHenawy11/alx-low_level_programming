e64ff7c6f3f91fefb76d8b806d7ca8e4f1a5d128
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-11 23:34:48 +0300

    Muse

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,59 +1,53 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
-* _strlen - it gives the length of a string
-*@s: pointer to a string
-* Return: the length of string
-*/
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*(s + i) != '\0')
-	{
-	i++;
-	}
-	return (i);
-}
-
-/**
-*new_dog - creates a new dog
-*@name: pointer to a string
-*@age: float
-*@owner: pointer to a string
-* Return: pointer to a variable
-*/
+ * new_dog - creates a new dog.
+ * @name: poppy's name.
+ * @age: poppy's age.
+ * @owner: poppy's owner.
+ * Return: a pointer.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i;
-	dog_t *d;
+	int x, y, z;
+	dog_t *ptr;
 
-	d = malloc(sizeof(dog_t));
-	if (d == '\0' || name == '\0' || owner == '\0')
+	ptr = malloc(sizeof(dog_t));
+
+	if (ptr == NULL)
 	{
-	return (0);
+		free(ptr);
+		return (0);
 	}
-	d->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	d->age = age;
-	d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if (d->name == '\0' || d->owner == '\0')
+
+	for (x = 0; name[x]; x++)
+		;
+	for (y = 0; owner[y]; y++)
+		;
+	ptr->name = malloc(x + 1);
+	ptr->owner = malloc(y + 1);
+
+
+	if (ptr->name == NULL || ptr->owner == NULL)
 	{
-	free(d->name);
-	free(d->owner);
-	free(d);
-	return (0);
+		free(ptr->name), free(ptr->owner), free(ptr);
+		return (NULL);
 	}
-	for (i = 0; name[i]; i++)
+	for (z = 0; z < x; z++)
 	{
-	d->name[i] = name[i];
+		ptr->name[z] = name[z];
 	}
-	d->name[i] = '\0';
-	for (i = 0; owner[i]; i++)
+	ptr->name[z] = '\0';
+	for (z = 0; z < y; z++)
 	{
-	d->owner[i] = owner[i];
+		ptr->owner[z] = owner[z];
 	}
-	d->owner[i] = '\0';
-	return (d);
+	ptr->owner[z] = '\0';
+	ptr->age = age;
+	return (ptr);
+
+
+
 }
