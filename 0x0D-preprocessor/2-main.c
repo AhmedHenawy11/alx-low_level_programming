c2e4cc6bdaf5cfad87902cce2c32c2cc055d4d6d
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-11 17:57:55 +0300

    daybreak's

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - print source file namde
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
