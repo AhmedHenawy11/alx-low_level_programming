29a8e26e37cebe1988d6b1a6ea04200cc7876dac
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-11 21:34:00 +0300

    cuss

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,11 +11,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog my_dog;
-
-	my_dog.name = name;
-	my_dog.age = age;
-	my_dog.owner = owner;
-
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 	return (0);
 }
