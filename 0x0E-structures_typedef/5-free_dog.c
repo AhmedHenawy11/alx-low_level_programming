e9a2d5c02ac84211ee8b45a8825b4ac5c752ab96
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-11 23:37:36 +0300

    federate

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs.
+ * @d: pointer.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+		{
+			free(d->name);
+		}
+		if (d->owner)
+		{
+			free(d->owner);
+		}
+
+		free(d);
+	}
+}
