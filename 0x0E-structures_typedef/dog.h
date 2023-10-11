f5800929e5b5db1e39cc84e950683fecbb5d7554
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-11 22:07:27 +0300

    Byrd's

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,13 @@
  * @owner: Third index
  * Description: description
  */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
-
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
