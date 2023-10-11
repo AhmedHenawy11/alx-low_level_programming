0ae0526c97b2d3aa471ff61e53c8e254383089cc
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-11 21:12:08 +0300

    lockers

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
+/**
+ * struct dog - dog informtion
+ * @name: First index
+ * @age: Second index
+ * @owner: Third index
+ * Description: description
+ */
 struct dog
 {
 	char *name;
