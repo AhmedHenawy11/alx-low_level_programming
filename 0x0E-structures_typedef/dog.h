1cc2f6d4dd09f19a5e992b2762488633130c5e91
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-11 21:14:04 +0300

    abased

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-
 /**
  * struct dog - dog informtion
  * @name: First index
@@ -12,12 +8,12 @@
  * @owner: Third index
  * Description: description
  */
-struct dog
-{
-	char *name;
-	int age;
-	char *owner;
-};
+	struct dog
+	{
+		char *name;
+		int age;
+		char *owner;
+	};
 
 
 #endif
