4a7d3e3066582eec73a37880b0c9e131f68e3739
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-13 17:09:23 +0300

    gregariousness's

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - printing a values of array
+ * @array: argument passed to another function by pointer.
+ * @action: pointer to a function .
+ * @size: defined size
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if ((*action) == NULL || array == NULL || size == NULL)
+	{
+		return;
+	}
+	int n = 0;
+
+	while (n < size)
+	{
+		action(array[n]);
+		n += 1;
+	}
+}
