48a537d0d253eecbb8802e5cffc4b85b45963bc4
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-14 17:27:54 +0300

    chessboard's stabilizes

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,17 +8,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
-	unsigned int i;
-	unsigned int sum = 0;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-	if (n == 0)
-	return (0);
-	va_start(args, n);
+	va_start(ap, n);
+
 	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(args, int);
-	}
-	va_end(args);
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
 	return (sum);
 }
