66d918be8d16d936b48e13c2f0ca90a286dabedf
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-14 19:21:53 +0300

    delicacy's Arieses

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(args, char*);
-		printf("%s", string);
+		if (string == NULL)
+		{
+			printf("(nill)");
+		}
+		else
+		{
+			printf("%s", string);
+		}
 		if (i < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
