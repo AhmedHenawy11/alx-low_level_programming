cb228e141690d797c5f3f66c268f86e1ff6accd7
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-14 17:28:39 +0300

    freeway's typo's

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,15 +8,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i, sum = 0;
+	va_list args;
+	unsigned int i;
+	unsigned int sum = 0;
 
-	va_start(ap, n);
-
+	if (n == 0)
+	return (0);
+	va_start(args, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-
-	va_end(ap);
-
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
 	return (sum);
 }
