1183967c4339b89bd1c222383b4c14ccbb846db8
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-14 19:17:53 +0300

    Doyle horology

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+# include <stdarg.h>
+/**
+ * print_strings - variadic function prints string
+ * @separator: passed char(comma)
+ * @n: the const number of arguments
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *string;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(args, char*);
+		printf("%s", string);
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
