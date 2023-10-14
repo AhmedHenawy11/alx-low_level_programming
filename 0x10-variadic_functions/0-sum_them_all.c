91164ce3b73650921739a2976f692d462e2f3d95
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-14 17:24:22 +0300

    Sheri cards

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,12 +8,12 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list args;
+	unsigned int i;
+	unsigned int sum = 0;
+	
 	if (n == 0)
 	return (0);
-	va_list args;
-	int i;
-	int sum = 0;
-
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
