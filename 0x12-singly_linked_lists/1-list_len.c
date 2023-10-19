9ae1ef33cf0e18d8f52507a21cf4217e2074cfe6
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-19 22:05:21 +0300

    quotes cohorts

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - check the code
+ * @h: passed head of linked list
+ * Return: count (length).
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
