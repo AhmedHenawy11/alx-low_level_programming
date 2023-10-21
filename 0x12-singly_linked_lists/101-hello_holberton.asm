b6306a6d5d43c877d74ee82055a0a568deafe41c
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-21 04:15:21 +0300

    undeserving whaling

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-global _start
-section .text
-_start:
-  ; Set the rax register to the system call number for `printf()`.
-  mov rax, 1
-  ; Set the rdi register to the address of the format string.
-  mov rdi, msg
-  ; Call the `printf()` system call.
-  syscall
-
-  ; Set the rax register to the system call number for `exit()`.
-  mov rax, 60
-  ; Exit the program with a status code of 0.
-  mov rdi, 0
-  syscall
-
-section .rodata
-msg: db "Hello, Holberton!\n", 0
+ global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
