0b1da34e2df7d23ac33e087911113e93beace7d6
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-24 00:22:37 -0400

    any Saddam

--- 0x0A-argc_argv/test.c
+++ 0x0A-argc_argv/test.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    for(int i = 0; i != argc ; i++)
-    {
-        printf("%s\n", argv[i]);
-    }
+	for (int i = 0; i != argc ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 }
