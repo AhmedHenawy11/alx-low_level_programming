5d61a56c6c8e358071987d3082c6813837e10a7b
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-10-31 04:12:06 +0200

    lubricate Noelle's

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* read_textfile -read and write from external file
+* @letters: number of chars
+* @filename: name of file
+* Return: if success
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t file, filer, filew;
+	char *str;
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+
+	str = malloc(sizeof(char) * letters);
+	if (str == NULL)
+	{
+		close(file);
+		return (0);
+	}
+
+	filer = read(file, str, letters);
+	if (filer == -1)
+	{
+		free(str);
+		close(file);
+		return (0);
+	}
+	filer = read(file, str, letters);
+	if (filer == -1)
+	{
+		free(str);
+		close(file);
+		return (0);
+	}
+	filew = write(STDOUT_FILENO, str, filer);
+	free(str);
+	close(file);
+	if (filew == -1 || filer != filew)
+	{
+		return (0);
+	}
+	return (filer);
+}
