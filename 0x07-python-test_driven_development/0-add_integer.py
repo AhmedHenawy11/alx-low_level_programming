ca3aaf43576188e29751ba02c7bbbc199ed1c2e6
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-01-08 20:49:16 +0200

    notations

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,12 +12,15 @@ def add_integer(a, b=98):
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
-        return
     if type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
-        return
     if type(a) is float:
         a = int(a)
     if type(b) is float:
         b = int(b)
     return a + b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
