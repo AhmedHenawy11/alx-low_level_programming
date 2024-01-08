e77cec18e5c675eaa592eb2cbcd2f70dc46a8a3a
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-01-08 04:14:15 +0200

    sharpshooter's

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+"""
+"0-add_integer" module.
+contains function that return sum of two integers
+"""
+
+
+def add_integer(a, b):
+    """Return the sum of two numbers"""
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
